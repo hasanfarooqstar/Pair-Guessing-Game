4bedfeeed33bf2b2bc5ba6687a4fe64d89bb76f5
hasanfarooqstar <102507210+hasanfarooqstar@users.noreply.github.com>
2022-07-28 11:54:15 +0500

    error handling and converted simple functions to arrow functions

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,26 +2,25 @@ import React, { useState } from "react";
 import Card from "./Card";
 
 function Cards() {
+  const defaultarry = [
+    { id: 1, img: "/img/Bulbasaur.png", status: "" },
+    { id: 1, img: "/img/Bulbasaur.png", status: "" },
+    { id: 2, img: "/img/ButterFree.png", status: "" },
+    { id: 2, img: "/img/ButterFree.png", status: "" },
+    { id: 3, img: "/img/Charmander.png", status: "" },
+    { id: 3, img: "/img/Charmander.png", status: "" },
+    { id: 4, img: "/img/Pickachu.png", status: "" },
+    { id: 4, img: "/img/Pickachu.png", status: "" },
+    { id: 5, img: "/img/Pidgetto.png", status: "" },
+    { id: 5, img: "/img/Pidgetto.png", status: "" },
+    { id: 6, img: "/img/Squirtle.png", status: "" },
+    { id: 6, img: "/img/Squirtle.png", status: "" },
+  ].sort(() => Math.random() - 0.5);
   const [lastScore, setlastScore] = useState(0);
   const [counter, setCounter] = useState(0);
   const [oldSelection, setOldSelection] = useState(-1);
-  const [items, setItems] = useState(
-    [
-      { id: 1, img: "/img/Bulbasaur.png", status: "" },
-      { id: 1, img: "/img/Bulbasaur.png", status: "" },
-      { id: 2, img: "/img/ButterFree.png", status: "" },
-      { id: 2, img: "/img/ButterFree.png", status: "" },
-      { id: 3, img: "/img/Charmander.png", status: "" },
-      { id: 3, img: "/img/Charmander.png", status: "" },
-      { id: 4, img: "/img/Pickachu.png", status: "" },
-      { id: 4, img: "/img/Pickachu.png", status: "" },
-      { id: 5, img: "/img/Pidgetto.png", status: "" },
-      { id: 5, img: "/img/Pidgetto.png", status: "" },
-      { id: 6, img: "/img/Squirtle.png", status: "" },
-      { id: 6, img: "/img/Squirtle.png", status: "" },
-    ].sort(() => Math.random() - 0.5)
-  );
-  function handleClick(arryindex) {
+  const [items, setItems] = useState([...defaultarry]);
+  const handleClick = (arryindex) => {
     items[arryindex].status = "active"; //jis Card pe click ho wo turn ho jaye
     setItems([...items]); // ... spread operator array ko update kry ga
     if (oldSelection === -1) {
@@ -31,10 +30,14 @@ function Cards() {
       checkMatch(arryindex);
     }
     setCounter(counter + 1);
-  }
-  function checkMatch(newSelection) {
+  };
+  const checkMatch = (newSelection) => {
     console.log("Check Match Function me enter ho gya hai");
-    if (items[oldSelection].id === items[newSelection].id) {
+    // oldSelection!==newSelection is used Because User may click the same-card(index) again
+    if (
+      oldSelection !== newSelection &&
+      items[oldSelection].id === items[newSelection].id
+    ) {
       setOldSelection(-1);
       setTimeout(() => {
         items[oldSelection].status = "cardhide";
@@ -49,15 +52,23 @@ function Cards() {
         setItems([...items]); // ... spread operator array ko update kry ga
       }, 1000);
     }
-  }
-  function restart() {
-    items.map((item, index) => {
-      items[index].status = "";
-    });
-    setItems([...items]);
+  };
+
+  const restart = () => {
+    setItems([...defaultarry]);
     setlastScore(counter);
     setCounter(0);
-  }
+  };
+
+  // restart function ka alternative Code
+  // const restart = () => {
+  //   items.map((item, index) => {
+  //     items[index].status = "";
+  //   });
+  //   setItems([...items]);
+  //   setlastScore(counter);
+  //   setCounter(0);
+  // };
 
   return (
     <>
